1ab918ca4720fe13caabc5fc373357025c14b298
Hack Reactor Student <student@hackreactor.com>
2017-07-10 19:23:27 -0700

    Refactor dancer and blinkydancer to pseudo

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,17 +4,17 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   //var oldStep = oldDancer.step;
+  //this.step(timeBetweenSteps);
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function(timeBetweenSteps) {
-    
-    // call the old version of step at the beginning of any call to this new version of step
-  Dancer.prototype.step(timeBetweenSteps);
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this, timeBetweenSteps);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
 };
